07ebb1abcaa343dc39cd0287b414ff6bb54dff4e
Raulgaray26 <raulgaray@uees.edu.ec>
2026-05-07 17:14:41 -0500

    actualizacion ejercicio 2

--- ejercicio1.pl
+++ ejercicio1.pl
@@ -4,3 +4,6 @@ tiene(juan, coche).
 tiene(ana, coche).
 
 tiene(pedro, scooter).
+
+tiene('Juan Lora', libro('Ana Karenina', 'Tolstoi', 'Luna', 2010)).
+tiene('Juan Lora', libro('El Principito', 'Antoine de Saint-Exupéry', 'Reynal & Hitchcock', 1943)).
