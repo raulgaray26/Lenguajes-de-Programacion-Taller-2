5afa301f63135b51dbfa8f65804bd40a5d86c95b
Raulgaray26 <raulgaray@uees.edu.ec>
2026-05-07 17:39:27 -0500

    actualizacion ejercicio 7

--- ejercicio1.pl
+++ ejercicio1.pl
@@ -18,3 +18,6 @@ factorial(N,R):-N1 is N - 1, factorial(N1,R1), R is N * R1.
 fibonacci(0,1).
 fibonacci(1,1).
 fibonacci(N,R):-N1 is N-1, N2 is N-2, fibonacci(N1,R1), fibonacci(N2,R2), R is R1 + R2.
+
+mcd(X,0,X).
+mcd(X,Y,M):-X1 is X mod Y, mcd(Y,X1,M).
