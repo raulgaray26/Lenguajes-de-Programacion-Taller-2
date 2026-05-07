5603777ae9e3a7f97618899ca5ebca27ef225da7
Raulgaray26 <raulgaray@uees.edu.ec>
2026-05-07 17:30:00 -0500

    actualizacion ejercicio 5

--- ejercicio1.pl
+++ ejercicio1.pl
@@ -9,3 +9,8 @@ tiene('Juan Lora', libro('Ana Karenina', 'Tolstoi', 'Luna', 2010)).
 tiene('Juan Lora', libro('El Principito', 'Antoine de Saint-Exupéry', 'Reynal & Hitchcock', 1943)).
 
 cuadrado(X,R):-R is X * X.
+
+media(A,B,M):-M is (A+B)/2.
+
+factorial(0,1).
+factorial(N,R):-N1 is N - 1, factorial(N1,R1), R is N * R1.
